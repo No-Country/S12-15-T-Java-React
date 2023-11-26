1934bb07b8cd2f3520d2e0ec481f203dfd2b4c55
Teradriel <luca.terzariol@outlook.com>
2023-11-26 21:31:56 +0100

    first commit

---
+++ backend/src/main/java/com/nocountry/S12G15/util/Constant.java
@@ -0,0 +1,10 @@
+package com.nocountry.S12G15.util;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public final class Constant {
+    public static final String API_VERSION = "v1/api";
+    public static final String RESOURCE_REGISTER= "/login";
+    public static final String RESOURCE_LOGIN= "/login";
+    public static final String RESOURCE_USER= "/users";
+}
---
+++ backend/src/test/java/com/nocountry/S12G15/S12G15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.S12G15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S12G15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
