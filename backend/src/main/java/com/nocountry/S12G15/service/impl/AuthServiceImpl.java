56bfaa07d2b545f33e0aea808e24fd3a88acfa1e
Teradriel <luca.terzariol@outlook.com>
2023-12-01 23:57:21 +0100

    feat: added security and jwt

--- backend/src/main/java/com/nocountry/S12G15/service/impl/AuthServiceImpl.java
+++ backend/src/main/java/com/nocountry/S12G15/service/impl/AuthServiceImpl.java
@@ -1,15 +1,18 @@
 package com.nocountry.S12G15.service.impl;
 
+import com.nocountry.S12G15.dto.request.AuthenticationRequest;
+import com.nocountry.S12G15.dto.response.AuthResponse;
 import com.nocountry.S12G15.service.AuthService;
 
 public class AuthServiceImpl implements AuthService {
+
     @Override
-    public Authresponse register(RegisterRequest request) {
+    public AuthResponse register(AuthenticationRequest request) {
         return null;
     }
 
     @Override
-    public AuthResponse authenticate(AuthenticationRequest request) {
+    public AuthResponse login(AuthenticationRequest request) {
         return null;
     }
 }
