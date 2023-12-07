f55ec9b8664ccbd7d81449398c22d0cc70ef48dd
mbernallaura <mbernallaura@gmail.com>
2023-12-07 16:27:31 -0500

    feat: hacer ruta home

---
+++ frontend/src/app/login/[id]/home/layout.js
@@ -0,0 +1,9 @@
+import styleHome from '@/styles/home/home.module.css';
+
+export default function RootLayout({ children }) {
+	return (
+		<main className={styleHome.home}>
+			{children}
+		</main>
+	);
+}
