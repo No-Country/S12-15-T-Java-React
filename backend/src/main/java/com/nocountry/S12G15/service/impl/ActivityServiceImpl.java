8c2029426504fdd2ab9fb7eec2445c64ccef69d7
Teradriel <luca.terzariol@outlook.com>
2023-12-08 23:43:59 +0100

    fix: annotation Activity Service

--- backend/src/main/java/com/nocountry/S12G15/service/impl/ActivityServiceImpl.java
+++ backend/src/main/java/com/nocountry/S12G15/service/impl/ActivityServiceImpl.java
@@ -10,12 +10,16 @@ import com.nocountry.S12G15.mapper.TaskMapper;
 import com.nocountry.S12G15.persistance.repository.ActivityRepository;
 import com.nocountry.S12G15.persistance.repository.TaskRepository;
 import com.nocountry.S12G15.service.ActivityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
 public class ActivityServiceImpl implements ActivityService {
 
     @Autowired
