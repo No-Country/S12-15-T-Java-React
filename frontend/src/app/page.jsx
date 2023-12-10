532257bc4a3544b17dab6155c3a5927668d4c42a
Luciano Ponce <poncestigaardluciano@gmail.com>
2023-12-10 10:42:25 -0300

    fix: cmobile configure responsive navbar

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,5 +1,8 @@
 import '@/styles/navbar.css';
 import '@/styles/Dropdown.css';
+import '@/styles/Flecha.css';
+import '@/styles/burguer.css';
+
 import Navbar from '@/components/Navbar';
 
 export const Home = () => {
