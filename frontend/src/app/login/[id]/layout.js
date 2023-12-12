d2828797ec489a153f469b60f1f24ba68f870d9f
mbernallaura <mbernallaura@gmail.com>
2023-12-11 21:22:07 -0500

    feat: estructura de rutas del proyecto

--- frontend/src/app/login/[id]/layout.js
+++ frontend/src/app/login/[id]/layout.js
@@ -1,4 +1,9 @@
 import Navbar from '@/components/Navbar';
+import '@/styles/navbar.css';
+import '@/styles/Dropdown.css';
+import '@/styles/Flecha.css';
+import '@/styles/burguer.css';
+
 
 export default function RootLayout({ children }) {
 	return (
