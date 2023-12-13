cde6e98b8c34c5e60282db63bacfe0b759486e44
OMAR GAVIRIA <omargaviria9@gmail.com>
2023-12-13 14:44:00 -0500

    style: delete some files

--- frontend/src/function/main.js
+++ frontend/src/function/main.js
@@ -1,11 +1,10 @@
-const SlideAsside=()=> {
+const SlideAsside = () => {
 	const btnToggle = document.querySelector('.toggle-btn');
 
-btnToggle.addEventListener('click', function () {
-	console.log('clik');
-	document.getElementById('sidebar').classList.toggle('active');
-	// console.log(document.getElementById('sidebar'));
-}); 
-	
+	btnToggle.addEventListener('click', function () {
+		console.log('clik');
+		document.getElementById('sidebar').classList.toggle('active');
+		// console.log(document.getElementById('sidebar'));
+	});
 };
 export default SlideAsside;
