1b34ba5140f01efd44f231c5a3caa7a3e075d59f
Andres Boni <andresboni91@gmail.com>
2023-12-19 13:39:28 +1300

    feat: fetch task columns, add new task column, delete task column

--- frontend/src/app/login/[id]/home/[idWorkspace]/board/[idBoard]/page.jsx
+++ frontend/src/app/login/[id]/home/[idWorkspace]/board/[idBoard]/page.jsx
@@ -1,11 +1,30 @@
+'use client';
+import { useEffect, useState } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 import { MdOutlineTableRows } from 'react-icons/md';
 import styleBoard from '@/styles/board/board.module.css';
 import Image from 'next/image';
 import ListBoard from '@/components/board/ListBoard';
 import { AddList } from '@/components/board/AddList';
+import { getTasks } from '@/app/api/workspace/board/boardApi';
 
-const Boardpage = () => {
+const Boardpage = ({ params }) => {
+	const [tasks, setTasks] = useState([]);
+	const { idBoard } = params;
+
+	useEffect(() => {
+		const fetchTasks = async () => {
+			try {
+				const response = await getTasks(idBoard);
+				setTasks(response.data);
+			} catch (error) {
+				console.error('Error fetching task columns data:', error);
+			}
+		};
+
+		fetchTasks();
+	}, [idBoard]);
+
 	return (
 		<div className={styleBoard.board}>
 			<header className={styleBoard.header}>
@@ -27,10 +46,17 @@ const Boardpage = () => {
 				</div>
 			</header>
 			<main className={styleBoard.tableBoard}>
-				<div>
-					<ListBoard name={'Title list'} />
-				</div>
-				<AddList />
+				{tasks &&
+					tasks.map((task) => (
+						<ListBoard
+							key={task.idTask}
+							task={task}
+							tasks={tasks}
+							setTasks={setTasks}
+						/>
+					))}
+
+				<AddList setTasks={setTasks} idBoard={idBoard} />
 			</main>
 		</div>
 	);
